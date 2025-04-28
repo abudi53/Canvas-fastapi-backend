efc52449fadfe06b23382294372aa2df3041e0e6
abudi53 <abdallahffh@gmail.com>
2025-04-27 20:54:26 -0400

    add generate_image structure and model. still controller and service left

---
+++ src/generate_image/model.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, ConfigDict, Field
+from uuid import UUID
+
+
+class ImageResponse(BaseModel):
+    id: UUID = Field(..., description="The unique identifier of the image.")
+    user_id: UUID = Field(
+        ..., description="The unique identifier of the user who uploaded the image."
+    )
+    prompt: str = Field(..., description="The prompt used to generate the image.")
+    image_base64: str = Field(
+        ..., description="The base64 encoded string of the generated image."
+    )
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class ImageRequest(BaseModel):
+    prompt: str = Field(..., description="The prompt used to generate the image.")
